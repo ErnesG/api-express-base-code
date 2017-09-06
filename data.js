2b1c6f9fc3942ad04d6088d2bacfdad13c2f7ac2
Ernesto Gon <egonzl92@outlook.com>
2017-09-05 22:48:48 -0500

    adds base project files

---
+++ data.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    "id": 1,
+    "first_name": "Ryan",
+    "last_name": "Chenkie",
+    "email": "ryan@scotch.io",
+    "website": "http://ryanchenkie.com"
+  },
+  {
+    "id": 2,
+    "first_name": "Chris",
+    "last_name": "Sevilleja",
+    "email": "chris@scotch.io",
+    "website": "http://scotch.io"
+  },
+  {
+    "id": 3,
+    "first_name": "Nick",
+    "last_name": "Cerminara",
+    "email": "nick@scotch.io",
+    "website": "http://scotch.io"
+  },
+  {
+    "id": 4,
+    "first_name": "Ado",
+    "last_name": "Kukic",
+    "email": "ado@scotch.io",
+    "website": "http://scotch.io"
+  },
+  {
+    "id": 5,
+    "first_name": "Holly",
+    "last_name": "Lloyd",
+    "email": "holly@scotch.io",
+    "website": "http://scotch.io"
+  }
+];
+// in order to use it in server.js we need to export this 
+module.exports = data;
